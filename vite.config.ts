b333d8facc49fb2763e83e7bc0490de897602736
Aric Walker <212087+aricwalker@users.noreply.github.com>
2025-08-06 02:02:42 +0000

    add gh-psges

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname;
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/hydrate-on-view-demo/", // Add this line to fix resource loading issues on GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
